dca26193664e268ec992dc2a16efbbe3694f0b97
noobvannoob234 <51087986+noobvannoob234@users.noreply.github.com>
2019-05-27 07:30:45 -0700

    Update main.cpp
    fixed issue preventing user from re-entering next hit loop on a new attack

--- main.cpp
+++ main.cpp
@@ -6,22 +6,23 @@ int armour;
 int pen;
 char nexthit; // another hit?
 char newattack; //make a new attack (reset all inputs)
-int damage(int d,int t,int a,int p); //variables in order: damage, toughness, armour, penetration.   returns damage as an integer in case needed for w/e
+int damage(int,int,int,int); //variables in order: damage, toughness, armour, penetration.   returns damage as an integer in case needed for w/e
 
 int main()
 {
     while (newattack != 'n')
     {
-		cout << "input Damage: ";
-		cin >> dam;
-		cout << "\n input Penetration: ";
-		cin >> pen;
-		cout << "\n input Armor: ";
-		cin >> armour;
-		cout << "\n input Toughness: ";
-		cin >> tough;
+	cout << "input Damage: ";
+	cin >> dam;
+	cout << "\n input Penetration: ";
+	cin >> pen;
+	cout << "\n input Armor: ";
+	cin >> armour;
+	cout << "\n input Toughness: ";
+	cin >> tough;
         damage(dam, tough, armour, pen);
-		while (nexthit != 'n')
+	nexthit = 'a'; //resets nexthit allowing re-entrance of below loop
+	while (nexthit != 'n')
         {
             cout << "\n  additional hits (retain tgh, arm, and pen)? (y/n) \n";
             cin >> nexthit;
